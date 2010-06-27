6c738de40e6b1129d19badfbc70787d4f1d216ed
Sho Hashimoto <hashimoto@shokai.org>
2010-06-27 12:14:14 +0900

    0.0.2 works on cygwin

--- lib/ImageResize/Image.rb
+++ lib/ImageResize/Image.rb
@@ -1,8 +1,10 @@
 module Image
   
-  
   def Image.resize(img_in, img_out, width, height)
-    resizer = File.dirname(__FILE__) + '/../../java/ImageResize.jar'
+    path = File.expand_path(File.dirname(__FILE__)).split(/\//)
+    path.pop
+    path.pop
+    resizer = "#{path.join('/')}/java/ImageResize.jar"
     puts `java -jar #{resizer} #{img_in} #{img_out} #{width} #{height}`
   end
   
--- samples/resize.rb
+++ samples/resize.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'ImageResize'
+#require '../lib/ImageResize'
 
 Image.resize('shokai-big.jpg', '~/shokai-small.jpg', 40, 40)
 
